59aae923ab2995b7b98fdcde27f863149cdd64c2
Andy Wright <atwright147@gmail.com>
2024-12-03 17:49:47 +0000

    solve day 1

--- src/2024/day-01/day-01.test.ts
+++ src/2024/day-01/day-01.test.ts
@@ -1,9 +1,13 @@
 import { describe, expect, it } from 'vitest';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import { day1 } from './day-01';
 
+const input = fs.readFileSync(path.join(__dirname, './day-01-input.txt'), 'utf-8');
+
 describe('day-01', () => {
-  it('should return 0', () => {
-    expect(day1('')).toBe(0);
+  it('should return the correct "distance"', () => {
+    expect(day1(input)).toBe(1666427);
   });
 });
--- src/2024/day-01/day-01.ts
+++ src/2024/day-01/day-01.ts
@@ -1,3 +1,19 @@
-export const day1 = (input: string) => {
-  return 0;
+export const day1 = (input: string): number => {
+  const trimmed = input.trimEnd();
+  const lines = trimmed.split('\n');
+  const left: number[] = [];
+  const right: number[] = [];
+
+  for (const line of lines) {
+    const [lineLeft, lineRight] = line.split(/\s+/);
+    left.push(Number(lineLeft));
+    right.push(Number(lineRight));
+  }
+
+  left.sort();
+  right.sort();
+
+  return left.reduce((prev, curr, index) => {
+    return prev + Math.abs(curr - right[index]);
+  }, 0);
 };
