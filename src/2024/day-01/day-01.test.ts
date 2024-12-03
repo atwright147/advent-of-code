f96ef9c6a0a7fde68f58e6d3cc94140b321b50c9
Andy Wright <atwright147@gmail.com>
2024-12-03 16:53:55 +0000

    initial set up

---
+++ src/2024/day-01/day-01.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+import { day1 } from './day-01';
+
+describe('day-01', () => {
+  it('should return 0', () => {
+    expect(day1('')).toBe(0);
+  });
+});
