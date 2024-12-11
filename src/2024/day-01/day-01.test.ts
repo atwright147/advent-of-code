da0534dee4f868935e174d5ae44e5b86e97da421
Andy Wright <atwright147@gmail.com>
2024-12-11 12:58:32 +0000

    add day 01, part 02

--- src/2024/day-01/day-01.test.ts
+++ src/2024/day-01/day-01.test.ts
@@ -2,12 +2,35 @@ import { describe, expect, it } from 'vitest';
 import fs from 'node:fs';
 import path from 'node:path';
 
-import { day1 } from './day-01';
+import { distanceApart, distanceSimilarity } from './day-01';
 
 const input = fs.readFileSync(path.join(__dirname, './day-01-input.txt'), 'utf-8');
 
-describe('day-01', () => {
-  it('should return the correct "distance"', () => {
-    expect(day1(input)).toBe(1666427);
+describe('Day 01', () => {
+  const exampleInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+  describe('distanceApart()', () => {
+    it('should return the correct "distance" for example input', () => {
+      expect(distanceApart(input)).toBe(1666427);
+    });
+
+    it('should return the correct "distance" for real input', () => {
+      expect(distanceApart(input)).toBe(1666427);
+    });
+  });
+
+  describe('distanceSimilarity()', () => {
+    it('should return the correct "similarity" example input', () => {
+      expect(distanceSimilarity(exampleInput)).toBe(31);
+    });
+
+    it('should return the correct "similarity" real input', () => {
+      expect(distanceSimilarity(input)).toBe(24316233);
+    });
   });
 });
--- src/2024/day-01/day-01.ts
+++ src/2024/day-01/day-01.ts
@@ -1,4 +1,4 @@
-export const day1 = (input: string): number => {
+export const distanceApart = (input: string): number => {
   const trimmed = input.trimEnd();
   const lines = trimmed.split('\n');
   const left: number[] = [];
@@ -15,5 +15,25 @@ export const day1 = (input: string): number => {
 
   return left.reduce((prev, curr, index) => {
     return prev + Math.abs(curr - right[index]);
+  }, 0);
+};
+
+export const distanceSimilarity = (input: string): number => {
+  const trimmed = input.trimEnd();
+  const lines = trimmed.split('\n');
+  const left: number[] = [];
+  const right: number[] = [];
+
+  for (const line of lines) {
+    const [lineLeft, lineRight] = line.split(/\s+/);
+    left.push(Number(lineLeft));
+    right.push(Number(lineRight));
+  }
+
+  left.sort();
+  right.sort();
+
+  return left.reduce((prev, curr) => {
+    return prev + curr * right.filter((item) => item === curr).length;
   }, 0);
 };
